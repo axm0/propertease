20939782d23e645ea2d5589b2cc9138f0a33ab24
gyang6633 <gyang6633@gmail.com>
2024-03-21 16:54:24 -0500

    created view properties page

--- public/index.php
+++ public/index.php
@@ -2,13 +2,19 @@
 // public/index.php
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
 require_once __DIR__ . '/../app/Controllers/AboutController.php';
+require_once __DIR__ . '/../app/Controllers/ViewPropertiesController.php';
 
 $path = $_SERVER['REQUEST_URI'];
 
 if ($path == '/propertease/public/about') {
     $controller = new AboutController();
     $controller->index();
-} else {
+}
+elseif ($path == '/propertease/public/viewProperties') {
+    $controller = new ViewPropertiesController();
+    $controller->index();
+}
+else {
     $controller = new HomeController();
     $controller->index();
 }
