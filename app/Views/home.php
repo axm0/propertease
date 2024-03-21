ce843debaf15c8854e0d9792b71e8526e564dc54
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-03-21 16:33:38 -0500

    3/21/2024

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,5 +3,5 @@
 function homePage() {
     echo "<h1>Welcome to PropertEase</h1>";
     echo "<p>Explore our property listings to find your dream home.</p>";
-    echo "<a href='/proprtease/public/about'>About Us</a>";
+    echo "<a href='/propertease/public/about'>About Us</a>";
 }
