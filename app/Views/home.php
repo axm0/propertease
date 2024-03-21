e77ba9dc3711631262880214160b0a1da1747555
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-03-20 20:46:43 -0500

    final commit 3/20/24

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,5 +3,5 @@
 function homePage() {
     echo "<h1>Welcome to HomeHub</h1>";
     echo "<p>Explore our property listings to find your dream home.</p>";
-    echo "<a href='/homehub/public/about'>About Us</a>"; // Add this line
+    echo "<a href='/homehub/public/about'>About Us</a>";
 }
