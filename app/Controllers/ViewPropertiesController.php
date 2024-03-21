0657c8c3b7b37afa7bcc83595caabd60420b7163
gyang6633 <gyang6633@gmail.com>
2024-03-21 17:05:00 -0500

    added line break and home to view properties page

--- app/Controllers/ViewPropertiesController.php
+++ app/Controllers/ViewPropertiesController.php
@@ -1,1 +1,10 @@
 <?php
+// app/Controllers/ViewPropertiesController.php
+require_once __DIR__ . '/../Core/Controller.php';
+require_once __DIR__ . '/../Views/viewProperties.php';
+
+class ViewPropertiesController extends Controller {
+    public function index() {
+        return viewPropertiesPage();
+    }
+}
--- app/Views/viewProperties.php
+++ app/Views/viewProperties.php
@@ -1,1 +1,7 @@
 <?php
+// app/Views/viewProperties.php
+function viewPropertiesPage() {
+    echo "<h1>View Properties Page</h1>";
+    echo "<p>Explore our property listings to find your dream home.</p>";
+    echo "<a href='/propertease/public/'>Home</a>";
+}
