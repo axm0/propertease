55e446a02e76591455dfba513b37eda4f15270bc
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-03-20 21:44:21 -0500

    final commit 3/20/24

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,7 @@
 <?php
 // app/Views/home.php
 function homePage() {
-    echo "<h1>Welcome to HomeHub</h1>";
+    echo "<h1>Welcome to PropertEase</h1>";
     echo "<p>Explore our property listings to find your dream home.</p>";
-    echo "<a href='/propertease/public/about'>About Us</a>";
+    echo "<a href='/proprtease/public/about'>About Us</a>";
 }
