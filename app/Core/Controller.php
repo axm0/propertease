88d5b31dac672a16631c836725e0d90684fbfd88
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-03-21 18:11:18 -0500

    3/21/2024

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -1,5 +1,15 @@
 <?php
 // app/Core/Controller.php
+
 class Controller {
-    // Base functionality that all controllers share
+    // This method can be used to load views
+    protected function view($viewName, $data = []) {
+        // Ensure the view file exists
+        if (file_exists('../app/Views/' . $viewName . '.php')) {
+            include '../app/Views/' . $viewName . '.php';
+        } else {
+            // Handle errors, possibly render an error page or throw an exception
+            die("View does not exist.");
+        }
+    }
 }
