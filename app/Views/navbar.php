909f436a56654a928510950f686dbcf50533139f
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-03-28 17:18:06 -0500

    commit message

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,58 +1,40 @@
-<!-- app/Views/navbar.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PropertEase</title>
+    <!-- Ensure Bootstrap CSS is included -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
-<body>
-<div class="navigation">
-    <div id="logo">
-        <a href="/propertease/public/home" class="menu-item">Home/Logo</a>
-    </div>
-    <div class="menu">
-        <a href="/propertease/public/viewProperties" class="menu-item">View Properties</a>
-        <a href="/propertease/public/myProperties" class="menu-item">My Properties</a>
-        <a href="/propertease/public/favorites" class="menu-item">My Favorites</a>
-        <a href="/propertease/public/about" class="menu-item">About</a>
-        <a href="/propertease/public/profile" class="menu-item">Profile</a>
-        <a href="#" class="menu-item" onclick="document.getElementById('id01').style.display='block'">Login/Sign-Up</a>
-    </div>
-</div>
-
-<div id="id01" class="login-modal">
-    <!-- Modal content -->
-    <form class="login-modal-content animate" action="/action_page.php" method="post">
-        <div class="imgcontainer">
-            <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <img src="img_avatar2.png" alt="Avatar" class="avatar">
+<body style="padding-top: 56px;"> <!-- Adjust this padding value to match the height of your navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a href="/propertease/public/home" class="navbar-brand">
+            <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Bootstrap">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="/propertease/public/viewProperties" class="nav-link">View Properties</a></li>
+                <li class="nav-item"><a href="/propertease/public/myProperties" class="nav-link">My Properties</a></li>
+                <li class="nav-item"><a href="/propertease/public/favorites" class="nav-link">My Favorites</a></li>
+                <li class="nav-item"><a href="/propertease/public/about" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="/propertease/public/profile" class="nav-link">Profile</a></li>
+                <li class="nav-item"><a href="/propertease/public/login" class="nav-link">Login/Sign-Up</a></li>
+            </ul>
         </div>
+    </div>
+</nav>
 
-        <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-            <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label>
-        </div>
-    </form>
-</div>
+<!-- Rest of your HTML content -->
 
-<script>
-    // Get the modal
-    var modal = document.getElementById('id01');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
+<!-- Include Bootstrap JS and Popper.js at the end of your document -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
