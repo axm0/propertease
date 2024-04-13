3795901be40262904ad21358ce94ceceb2856499
Abdul Aziz Mohammed <abdulaziz.x.mohammed@gmail.com>
2024-04-13 14:33:10 -0500

    made sagnificant changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 // /public/js/main.js
 document.addEventListener('DOMContentLoaded', function() {
-    // When the logo is clicked, redirect to the homepage
     document.getElementById('logo').addEventListener('click', function() {
         window.location.href = '/';
     });
