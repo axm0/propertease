f151596de3aab21754242801510f9a48ec8dee72
gyang6633 <gyang6633@gmail.com>
2024-04-16 15:24:08 -0500

    added actual photos and info to all properties page

--- app/Controllers/ViewPropertiesController.php
+++ app/Controllers/ViewPropertiesController.php
@@ -1,10 +1,33 @@
 <?php
 
 require_once __DIR__ . '/../Core/Controller.php';
+if (file_exists(__DIR__ . '/../../config/config.php')) {
+    require_once __DIR__ . '/../../config/config.php';
+} else {
+    die('Failed to load configuration file');
+}
 
 
 class ViewPropertiesController extends Controller {
     public function index() {
-        $this->view('viewproperties');
+        $images = $this->getAllProperties();
+        $this->view('viewProperties', ['images' => $images]);
+    }
+
+    private function getAllProperties() {
+        $sql = "SELECT p.PropertyURL, p.PropertyID, pr.Address, pr.Description
+            FROM Photos p
+            JOIN Property pr ON p.PropertyID = pr.PropertyID
+            ORDER BY p.PropertyID";
+        $result = $this->db->query($sql);
+        $images = [];
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $images[$row['PropertyID']]['images'][] = $row['PropertyURL'];
+                $images[$row['PropertyID']]['address'] = $row['Address'];
+                $images[$row['PropertyID']]['description'] = $row['Description'];
+            }
+        }
+        return $images;
     }
 }
