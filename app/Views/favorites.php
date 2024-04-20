f7cc296935b10b3fc0526fec4ff58f0c730f422e
Abdul Aziz Mohammed <144386480+axm0@users.noreply.github.com>
2024-04-20 17:59:01 -0500

    Save work before updating branch

--- app/Views/favorites.php
+++ app/Views/favorites.php
@@ -12,6 +12,7 @@ require_once 'navbar.php';
         <link rel="stylesheet" href="/css/style.css">
         <link rel="stylesheet" href="/css/myProperties.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <script src="https://kit.fontawesome.com/03397d1206.js" crossorigin="anonymous"></script>
         <title>My Favorites</title>
     </head>
--- app/Views/myProperties.php
+++ app/Views/myProperties.php
@@ -19,6 +19,7 @@ if (!isset($_SESSION['user_name'])) {
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/myProperties.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <title>My Properties</title>
 </head>
 <body>
